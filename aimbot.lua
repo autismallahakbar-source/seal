8ea551c5b96604f681f41d648ff754344e1c7551
autismallahakbar-source <autismallahakbar@gmail.com>
2026-09-21 21:42:45 +0700

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -24,7 +24,7 @@ local TriggerbotTab = Window:CreateTab("Triggerbot", nil)
 local ESPTab = Window:CreateTab("ESP", nil)
 local ChecksTab = Window:CreateTab("Checks", nil)
 local MovementTab = Window:CreateTab("Movement", nil)
-local Seal's photoTab = Window:CreateTab("Seal", nil)
+local SealPhotoTab = Window:CreateTab("Seal", nil)
 
 -- ===== СЕРВИСЫ =====
 local Players = game:GetService("Players")
